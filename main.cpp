195541cabb0bed58aa15db2529d92e494517d256
bunnerman <parnilrbalawade@gmail.com>
2026-04-09 22:04:11 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,46 +6,65 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
-#include <ctime>
-#include <cstdlib>
 #include <random>
+#include <chrono>
+#include <cstdint>
+#include <typeinfo> //? For Debugging
 
-using std::cout;
-using std::cin;
-using std::vector;
-using std::string;
+// RNG Algorithm with balance of fast and good
+struct PCG32 //! No touching, write documentation later
+{
+	uint64_t state;
+    uint64_t inc;
 
-#include <string> // unnecessary but keep for now
+    PCG32(uint64_t seed, uint64_t seq = 1) 
+	{
+        state = 0;
+        inc = (seq << 1u) | 1u;
+        next();
+        state += seed;
+        next();
+    }
+
+    uint32_t next() 
+	{
+        uint64_t oldstate = state;
+        state = oldstate * 6364136223846793005ULL + inc;
+
+        uint32_t xorshifted =
+            (uint32_t)(((oldstate >> 18u) ^ oldstate) >> 27u);
+        uint32_t rot = oldstate >> 59u;
+
+        return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+    }
+};
 
 //-------------------------------//
 //           FUNCTIONS           //
 //-------------------------------//
 
-vector<vector<int>> generateGradientGrid(float res)
+std::vector<std::vector<int>> generateGradientGrid(PCG32& rng, float res)
 {
 	int resolution = (int) res;
-    vector<vector<int>> gradientGrid((int)resolution);
-	for (int i = 0; i < resolution; i++)
-		gradientGrid[i].resize(resolution);
+    std::vector<std::vector<int>> gradientGrid(resolution, std::vector<int>(resolution));
 
     for (int i = 0; i < resolution; i++)
         for (int j = 0; j < resolution; j++)
-            gradientGrid[i][j] = rand() % 8;
-			// 8 Cardinal Directions || East = 0 degrees = 0 index, NE = 45 degrees = 1 index, and so on 
+            gradientGrid[i][j] = rng.next() & 15; // bitwise is faster for powers of 2
 
     return gradientGrid;
 }
 
 
 
-void displayGradientVectors(vector<vector<int>> &gradientGrid)
+void displayGradientVectors(std::vector<std::vector<int>> &gradientGrid)
 {
 	int resolution = gradientGrid.size();
-	for (int i = 0; i < resolution; i++)
+	for (std::vector<int> &i : gradientGrid)
 	{
-		for (int j = 0; j < resolution; j++)
-			cout << gradientGrid[i][j] << " ";
-		cout << "\n";
+		for (int &j : i)
+			std::cout << j << " ";
+		std::cout << "\n";
 	}
 }
 
@@ -58,29 +77,29 @@ float dotProduct(int gradientIndex, float dx, float dy)
 		cos θ represents x component as it has it's peak values at 0 and 180
 		sin θ represents y component as it has it's peak values at 90 and 270
 
-		θ is derived from the gradient index, 0 = 0 degrees, 1 = 45 degrees, so on. 
+		θ is derived from the gradient index, 0 = 0 degrees, 1 = 22.5 degrees, so on. 
 		East (Positive X) = 0 degrees
 	*/
 	float gradientX;
 	float gradientY;
 	switch(gradientIndex)
 	{
-		case 0:
-			gradientX =  1.0f; gradientY =  0.0f; break;
-		case 1:
-			gradientX =  0.70710678f; gradientY =  0.70710678f; break;
-		case 2:
-			gradientX =  0.0f; gradientY =  1.0f; break;
-		case 3:
-			gradientX = -0.70710678f; gradientY =  0.70710678f; break; // eg: gx = cos(135), gy = sin(135) 
-		case 4:
-			gradientX = -1.0f; gradientY =  0.0f; break;
-		case 5:
-			gradientX = -0.70710678f; gradientY = -0.70710678f; break;
-		case 6:
-			gradientX =  0.0f; gradientY = -1.0f; break;
-		case 7:
-			gradientX =  0.70710678f; gradientY = -0.70710678f; break;
+		case 0:  gradientX =  1.00000000f; gradientY =  0.00000000f; break;
+		case 1:  gradientX =  0.92387953f; gradientY =  0.38268343f; break;
+		case 2:  gradientX =  0.70710678f; gradientY =  0.70710678f; break;
+		case 3:  gradientX =  0.38268343f; gradientY =  0.92387953f; break;
+		case 4:  gradientX =  0.00000000f; gradientY =  1.00000000f; break;
+		case 5:  gradientX = -0.38268343f; gradientY =  0.92387953f; break;
+		case 6:  gradientX = -0.70710678f; gradientY =  0.70710678f; break;
+		case 7:  gradientX = -0.92387953f; gradientY =  0.38268343f; break;
+		case 8:  gradientX = -1.00000000f; gradientY =  0.00000000f; break;
+		case 9:  gradientX = -0.92387953f; gradientY = -0.38268343f; break;
+		case 10: gradientX = -0.70710678f; gradientY = -0.70710678f; break;
+		case 11: gradientX = -0.38268343f; gradientY = -0.92387953f; break;
+		case 12: gradientX =  0.00000000f; gradientY = -1.00000000f; break;
+		case 13: gradientX =  0.38268343f; gradientY = -0.92387953f; break;
+		case 14: gradientX =  0.70710678f; gradientY = -0.70710678f; break;
+		case 15: gradientX =  0.92387953f; gradientY = -0.38268343f; break;
 	}
 
 	return gradientX * dx + gradientY * dy;
@@ -100,7 +119,7 @@ float linearInterpolation(float a, float b, float t)
 
 
 
-float generatePerlinValue(float x, float y, vector<vector<int>> &gradientGrid, int mapSize)
+float generatePerlinValue(float x, float y, std::vector<std::vector<int>> &gradientGrid, int mapSize)
 {
 	int gradientResolution = gradientGrid.size();
 	float scale = (float) gradientResolution / mapSize;
@@ -124,23 +143,23 @@ float generatePerlinValue(float x, float y, vector<vector<int>> &gradientGrid, i
 	int y1 = (gy + 1) % gradientResolution;
 
 	// Distance Vectors | (0, 0) = Bottom-Left Grid Corner
-	float dx00 = lx;
-	float dy00 = ly;
+	float dvx00 = lx;
+	float dvy00 = ly;
 
-	float dx10 = lx - 1;
-	float dy10 = ly;
+	float dvx10 = lx - 1;
+	float dvy10 = ly;
 
-	float dx01 = lx;
-	float dy01 = ly - 1;
+	float dvx01 = lx;
+	float dvy01 = ly - 1;
 
-	float dx11 = lx - 1;
-	float dy11 = ly - 1;
+	float dvx11 = lx - 1;
+	float dvy11 = ly - 1;
 
 	// Dot Products
-	float dotProduct00 = dotProduct(gradientGrid[y0][x0], dx00, dy00);
-	float dotProduct10 = dotProduct(gradientGrid[y0][x1], dx10, dy10);
-	float dotProduct01 = dotProduct(gradientGrid[y1][x0], dx01, dy01);
-	float dotProduct11 = dotProduct(gradientGrid[y1][x1], dx11, dy11);
+	float dotProduct00 = dotProduct(gradientGrid[y0][x0], dvx00, dvy00);
+	float dotProduct10 = dotProduct(gradientGrid[y0][x1], dvx10, dvy10);
+	float dotProduct01 = dotProduct(gradientGrid[y1][x0], dvx01, dvy01);
+	float dotProduct11 = dotProduct(gradientGrid[y1][x1], dvx11, dvy11);
 
 	// Perlin Fade Function
 	float u = perlinFadeFunction(lx);
@@ -155,20 +174,20 @@ float generatePerlinValue(float x, float y, vector<vector<int>> &gradientGrid, i
 	return value;
 }
 
-void writeNoiseGridToFile(vector<vector<float>> &noiseGrid)
+void writeNoiseGridToFile(std::vector<std::vector<float>> &noiseGrid)
 {
 	std::ofstream outputFile("outputFile.txt");
 	if (!outputFile)
 	{
-		cout << "ERROR! FILE NOT ABLE TO BE OPENED";
+		std::cout << "ERROR! FILE NOT ABLE TO BE OPENED";
 		return;
 	}
     int mapSize = noiseGrid.size();
 
-    for (int i = 0; i < mapSize; i++)
+    for (auto &i : noiseGrid)
     {
-        for (int j = 0; j < mapSize; j++)
-            outputFile << noiseGrid[i][j] << " ";
+        for (float &j : i)
+            outputFile << j << " ";
         outputFile << "\n";
     }
 
@@ -181,22 +200,22 @@ void writeNoiseGridToFile(vector<vector<float>> &noiseGrid)
 
 int main()
 {
-	std::srand(std::time(nullptr));
-
+	auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count(); // long long int
+	PCG32 rng(seed);
 	int baseGradientResolution = 4; // 4
 	int mapSize = 512; // 512
-	int octaves = 6; // 6
+	int octaves = 6; // 64
 	float persistence = 0.5f; // 0.5f - amplitude, influence
 	float lacunarity = 2.0f; // 2.0f - frequency, detail-gain
 
-	vector<vector<vector<int>>> gradientGrids(octaves);
-	vector<vector<float>> noiseGrid(mapSize);
+	std::vector<std::vector<std::vector<int>>> gradientGrids(octaves);
+	std::vector<std::vector<float>> noiseGrid(mapSize);
 
 
 	float currentRes = baseGradientResolution;
 	for (int i = 0; i < octaves; i++)
 	{
-		gradientGrids[i] = generateGradientGrid(currentRes);
+		gradientGrids[i] = generateGradientGrid(rng, currentRes);
 		currentRes *= lacunarity;
 	}
 
@@ -213,7 +232,7 @@ int main()
 			for (int k = 0; k < octaves; k++)
 			{
 				value += generatePerlinValue((float) j, (float) i, gradientGrids[k], mapSize) * amplitude;
-				// i and j swappred, see documentation
+				// i and j swapped, see documentation
 				amplitude *= persistence;
 			}
 			if (value < minValue)
